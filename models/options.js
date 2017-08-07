8905c4d3d553620b644eb57417b92eedb3575fea
Robert Hutchins <rjhutchins22@gmail.com>
2017-08-07 16:46:24 -0400

    added images and descriptions to options

--- models/options.js
+++ models/options.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const optionSchema = new mongoose.Schema({
   label: {type: String},
   id:{type: String},
-  optionPull: {type: String}
+  optionPull: {type: String},
+  image_before: {type: String},
+  image_after: {type: String},
+  product_description: {type: String}
 })
 
 const Option = mongoose.model('Option', optionSchema);
